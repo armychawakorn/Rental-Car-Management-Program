87235482ec0cccd1934b7a93020273a096a8f068
RosiRosinante <armychawakorn@gmail.com>
2023-04-07 17:24:59 +0700

    Update

--- Class/Car.cs
+++ Class/Car.cs
@@ -8,26 +8,26 @@ namespace Rental_Car_Management_Program.Class
 {
     public class Car
     {
-        private string CarName;
-        private string CarID;
-        private decimal price;
+        private string CarModel;
+        private string licensePlate;
+        private decimal pricePerDay;
         public Car(string CarName, string CarID, decimal price)
         {
-            this.CarName = CarName;
-            this.CarID = CarID;
-            this.price = price;
+            this.CarModel = CarName;
+            this.licensePlate = CarID;
+            this.pricePerDay = price;
         }
         public string getCarName()
         {
-            return this.CarName;
+            return this.CarModel;
         }
         public string getCatID()
         {
-            return this.CarID;
+            return this.licensePlate;
         }
         public decimal getPrice()
         {
-            return price;
+            return pricePerDay;
         }
     }
 }
